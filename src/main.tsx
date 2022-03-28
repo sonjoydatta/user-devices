111b6f8fc66f9e10e54eb3fb541f427d2c985713
Sonjoy Datta <sonjoy.dat@gmail.com>
2022-03-28 15:42:05 +0600

    Added theming feature with SC

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import GlobalStyle, { defaultTheme } from './styles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<React.StrictMode>
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const defaultTheme: DefaultTheme = {
+	colors: {
+		primary: '#1890ff',
+		success: '#1ad743',
+		danger: '#d87675',
+		dark: '#121f3e',
+		text: '#4f4e4e',
+		'gray-100': '#f9f9f9',
+		'gray-200': '#e9ecef',
+	},
+};
